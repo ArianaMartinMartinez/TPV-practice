90664197e0910d3999661b8d6e75cdf3e5fe7b04
Ariana Martin <arianamartinmartinez@gmail.com>
2023-07-25 17:10:48 +0200

    feat: backend project created

---
+++ backend/tpv-practice/src/main/java/com/tpv/tpvpractice/TpvPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.tpv.tpvpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpvPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpvPracticeApplication.class, args);
+	}
+
+}
---
+++ backend/tpv-practice/src/test/java/com/tpv/tpvpractice/TpvPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpv.tpvpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpvPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
